5223f247c9f411558466289fc36e4b5c51a4e7da
waqarkaleemkhan <waqar.musa777@gmail.com>
2021-04-04 21:50:57 +0500

    url is updated for update_item

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.store,name='store'),
     path('cart/',views.cart,name='cart'),
-    path('checkout/',views.checkout,name='checkout')
+    path('checkout/',views.checkout,name='checkout'),
+    path('update_item/',views.updateItem,name='update_item')
 ]
