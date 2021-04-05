80fbe4ddd6c8eb4baa56752b8fd6a80d8acd944d
waqarkaleemkhan <waqar.musa777@gmail.com>
2021-04-05 23:19:00 +0500

    shipping is equal to false for non user

--- store/views.py
+++ store/views.py
@@ -12,7 +12,7 @@ def store(request):
         cartItems=order.get_cart_items
     else:
         items = []
-        order ={'get_cart_total':0,'get_cart_items':0}
+        order ={'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems=order['get_cart_items']
     products=Product.objects.all()
     context = {'products':products,'cartItems':cartItems}
@@ -26,7 +26,7 @@ def cart(request):
         cartItems=order.get_cart_items
     else:
         items = []
-        order ={'get_cart_total':0,'get_cart_items':0}
+        order ={'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems=order['get_cart_items']
     context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html',context)
@@ -40,7 +40,7 @@ def checkout(request):
         cartItems=order.get_cart_items
     else:
         items = []
-        order ={'get_cart_total':0,'get_cart_items':0}
+        order ={'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems=order['get_cart_items']
     context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/checkout.html',context)
